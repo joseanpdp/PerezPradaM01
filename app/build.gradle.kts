001633c748f099e0bfb5a944a7c3db85ed834a37
jose <joseanperezdp@gmail.com>
2023-12-15 08:54:46 +0100

    Corregido error con el paquete com.vedruna.perezdepradae01 porque tenia configurado com.example.perezdepradae01. También se ha añadido una firma debajo de los botones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.perezdepradae01"
+        applicationId = "com.vedruna.perezdepradae01"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
